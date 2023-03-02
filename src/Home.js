4856ce13f27032fd1ca42348a7683040d3440fe3
Jesse Lloyd <116366380+jlloyd0430@users.noreply.github.com>
2023-03-02 09:10:36 -0500

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,78 +1,161 @@
-
-import { useState, useEffect } from "react";
-
-import "./App.css";
-
-import img1 from "./images/img1.JPG";
-import img2 from "./images/img2.JPG";
-import img3 from "./images/img3.JPG";
-import img4 from "./images/img4.JPG";
-import img5 from "./images/img5.JPG";
-import img6 from "./images/img6.JPG";
-import img7 from "./images/img7.JPG";
-import img8 from "./images/img8.JPG";
-import img9 from "./images/img9.JPG";
-import img10 from "./images/img10.JPG";
-import img11 from "./images/img11.JPG";
-
-function Contact() {
-  const [currentImg, setCurrentImg] = useState(0);
-
-  const images = [
-    { src: img1, alt: "Image 1" },
-    { src: img2, alt: "Image 2" },
-    { src: img3, alt: "Image 3" },
-    { src: img4, alt: "Image 4" },
-    { src: img5, alt: "Image 5" },
-    { src: img6, alt: "Image 6" },
-    { src: img7, alt: "Image 7" },
-    { src: img8, alt: "Image 8" },
-    { src: img9, alt: "Image 9" },
-    { src: img10, alt: "Image 10" },
-    { src: img11, alt: "Image 11" },
-  ];
-
-  useEffect(() => {
-const interval = setInterval(() => {
- 
-      setCurrentImg((currentImg + 1) % images.length);
-    }, 2500);
-    return () => clearInterval(interval);
-},[currentImg]);
-
-  return (
-    <>
-
-      <div id="text-wrapper2">
-        {images.map((image, index) => (
-          <img
-            className="home-img"
-            key={index}
-            src={image.src}
-            alt={image.alt}
-            style={{ display: currentImg === index? “block”  : “none” }}
-          />
-        ))}
-      </div>
-
-      <h2>Mungovan Trucking .llc</h2>
-
-      <div className="text-wrapper">
-        <p></p>
-        <p>
-          Mungovan.land is an application made by Mungovan Trucking employees
-          and affiliates exclusivley with the primary purpose of providing
-          resources and tools to make the day to day job a little bit easier.
-        </p>
-        <p className="price">Beta version1.0</p>
-
-        <p className="disc">
-          Disclosure: This application is still under construction, if you wish
-          to make an inquiry just message me at 7748135597
-        </p>
-      </div>
-    </>
-  );
+body {
+  background-color: green !important;
+  margin: auto;
+}
+.searchbar {
+  margin-bottom: 10px;
+  display: flex;
+  justify-self: center;
+  padding: 7px;
+  border-radius: 25px;
+}
+.col-md-6 {
+  font-family: 'Lobster', cursive;
+}
+header {
+  font-family: 'Lobster', cursive;
 }
 
-export default Contact;
+h2 {
+text-align: center;
+}
+
+.date-section {
+  display: flex;
+  flex-direction: column;
+  color: #333;
+  /* margin-top: 1rem; */
+  max-width: 200px;
+  height: 4rem;
+}
+.date-section label {
+  margin-bottom: 0.5rem;
+}
+nav {
+  text-align: right;
+}
+h1 {
+  color: grey;
+}
+a {
+  display: inline-block;
+  color: white;
+}
+a:hover {
+  color: yellow;
+  text-decoration: underline;
+}
+nav a {
+  padding: 10px 20px;
+}
+form,
+h1 {
+  margin-top: 15px;
+  color: green;
+}
+h2 {
+  color: #fff;
+}
+form {
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  width: 60%;
+  background-color: white;
+  padding: 1rem;
+  border-radius: 16px;
+}
+.name-section {
+  display: flex;
+  gap: 5rem;
+  flex: 3;
+  margin-bottom: 1rem;
+  justify-content: space-between;
+}
+.date-section {
+  flex: 1;
+}
+#text-wrapper2 {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  margin-top: 2rem;
+  margin: auto;
+}
+h2 {
+  text-align: center;
+}
+#text-wrapper2 img {
+  margin: 20px;
+  max-width: 450px;
+  height: auto;
+  /* margin: 0.1rem; */
+}
+.text-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 2rem;
+}
+.text-wrapper h2 {
+  text-align: center;
+}
+.text-wrapper p {
+  text-align: center;
+  margin-top: 1rem;
+}
+.price {
+  font-weight: bold;
+}
+.cta {
+  margin-top: 2rem;
+}
+
+.home-img {
+  display: block;
+  margin: 10px;
+  width: 100%;
+  height: auto;
+  border-radius: 10px;
+  border: 2px solid #ccc;
+}
+.book-now-btn {
+  padding: 1rem 2rem;
+  background-color: #4CAF50;
+  color: white;
+  text-decoration: none;
+  border-radius: 5px;
+}
+.disc {
+  text-align: center;
+  margin-top: 2rem;
+  font-size: 0.8rem;
+}
+.icons {
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  width: 75%;
+}
+
+@media only screen and (max-width: 768px) {
+  form {
+    width: 90% !important;
+    margin: auto !important;
+  }
+
+  .home-img {
+width: 100%;
+  }
+
+  /* Add some margin to the images to create spacing between them */
+#text-wrapper2 {
+  flex-direction: column;
+  align-items: center;
+}
+#text-wrapper2 img {
+  margin: 20px 0;
+}
+
