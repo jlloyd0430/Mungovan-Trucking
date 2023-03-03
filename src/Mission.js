9866636e00797a50df1c3c4f53008187d7af5ab4
Jesse Lloyd <116366380+jlloyd0430@users.noreply.github.com>
2023-03-03 08:10:06 -0500

    Update Mission.js

--- src/Mission.js
+++ src/Mission.js
@@ -1,7 +1,183 @@
 import React, { useState } from "react";
 import "./AddressBook.css";
 
-const addresses = [
+  
+  const addresses = [
+    
+  {
+    town: "Acton",
+    streetAddress: "42 Massachusetts ave",
+    state: "MA",
+    zip: "01720",
+  },
+  {
+    town: "Amesbury",
+    streetAddress: "13 pleasant st",
+    state: "MA",
+    zip: "01913",
+  },
+  {
+    town: "Amherst",
+    streetAddress: "20 fearing st",
+    state: "MA",
+    zip: "01002",
+  },
+  {
+    town: "Andover",
+    streetAddress: "27 elm st",
+    state: "MA",
+    zip: "01810",
+  },
+  {
+    town: "Arlington",
+    streetAddress: "67 pleasant st",
+    state: "MA",
+    zip: "02476",
+  },
+  {
+    town: "Ashburnham",
+    streetAddress: "pleasant st",
+    state: "MA",
+    zip: "01430",
+  },
+  {
+    town: "Ashby",
+    streetAddress: "south rd",
+    state: "MA",
+    zip: "01431",
+  },
+  {
+    town: "Ashfield",
+    streetAddress: "268 main st",
+    state: "MA",
+    zip: "01330",
+  },
+  {
+    town: "Ashland",
+    streetAddress: "366 main st",
+    state: "MA",
+    zip: "01721",
+  },
+  {
+    town: "Assonet",
+    streetAddress: "main st",
+    state: "MA",
+    zip: "02702",
+  },
+  {
+    town: "Athol",
+    streetAddress: "58 riverbend st",
+    state: "MA",
+    zip: "01331",
+  },
+  {
+    town: "Attleboro",
+    streetAddress: "39 bank st",
+    state: "MA",
+    zip: "02703",
+  },
+  {
+    town: "Auburn",
+    streetAddress: "leicester & garden st",
+    state: "MA",
+    zip: "01501",
+  },
+  {
+    town: "Ayer",
+    streetAddress: "26 pleasant st",
+    state: "MA",
+    zip: "01432",
+  },
+  {
+    town: "Adams",
+    streetAddress: "20 Maple st",
+    state: "MA",
+    zip: "01220",
+  },
+  {
+    town: "Barnstable",
+    streetAddress: "3240 main st",
+    state: "MA",
+    zip: "02630",
+  },
+  {
+    town: "Barre",
+    streetAddress: "james st",
+    state: "MA",
+    zip: "01005",
+  },
+  {
+    town: "Bass river",
+    streetAddress: "45 station ave",
+    state: "MA",
+    zip: "02664",
+  },
+  {
+    town: "Becket",
+    streetAddress: "washington st",
+    state: "MA",
+    zip: "01223",
+  },
+  {
+    town: "Bedford",
+    streetAddress: "70 page rd",
+    state: "MA",
+    zip: "01730",
+  },
+  {
+    town: "Belchertown",
+    streetAddress: "35 jabish st",
+    state: "MA",
+    zip: "01007",
+  },
+  {
+    town: "Belmont",
+    streetAddress: "115 Leonard St",
+    state: "MA",
+    zip: "02478",
+  },
+  {
+    town: "Berlin",
+    streetAddress: "Highland St",
+    state: "MA",
+    zip: "01503",
+  },
+  {
+    town: "Bernardston",
+    streetAddress: "23 Church St",
+    state: "MA",
+    zip: "01337",
+  },
+  {
+    town: "Beverly",
+    streetAddress: "15 Elliot St",
+    state: "MA",
+    zip: "01915",
+  },
+  {
+    town: "Billerica",
+    streetAddress: "9 Andover Rd",
+    state: "MA",
+    zip: "01821",
+  },
+  {
+    town: "Blandford",
+    streetAddress: "4 Glasgow Rd",
+    state: "MA",
+    zip: "01008",
+  },
+  {
+    town: "Bolton",
+    streetAddress: "760 Main St",
+    state: "MA",
+    zip: "01740",
+  },
+  {
+    town: "Boston",
+    streetAddress: "6 Harrison Ave",
+    state: "MA",
+    zip: "02111",
+  },
   {
     town: "Clinton",
     streetAddress: "223 Chestnut st",
