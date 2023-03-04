123654e5d9d1ac1a6ab1e90ab52845581c551139
Jesse Lloyd <116366380+jlloyd0430@users.noreply.github.com>
2023-03-04 11:33:17 -0500

    Update Gallery.js

--- src/Gallery.js
+++ src/Gallery.js
@@ -6,20 +6,28 @@ import useAxios from "./hooks/useAxios";
 function Gallery() {
   const [setUrl, data, loading, setLoading] = useAxios();
 
-  useEffect(() => {
-    setUrl(
-      "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=Lc6mCmy8pmn55pfWyTeOUCytfdZvsJsUqRhtowWL"
-    );
-    setLoading(true);
-  }, []);
+  // useEffect(() => {
+  //   setUrl(
+  //     "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=Lc6mCmy8pmn55pfWyTeOUCytfdZvsJsUqRhtowWL"
+  //   );
+  //   setLoading(true);
+  // }, []);
 
   return (
     <div className="col-12">
-      <h2>Gallery</h2>
+      <h1>Services</h1>
+      <h2>Trucking</h2>
       <p>
-        These are the last known images the Rover had taken before going
-        offline.
+        We operate 26ft straight trucks/ box trucks because they provide ample
+        capacity while being small enough to deliver to most location in the
+        tight spaces of New England cities. Typically 12 standard pallets fit on
+        our trucks which are equipped with liftgates for ground deliveries.
       </p>
+      <h2>Warehousing</h2>
+      <p></p>
+      <h2>Recycling</h2>
+      <p></p>
+      <h2></h2>
       <div className="row">
         {!loading && data ? (
           data.photos.map((photo) => <Photo key={photo.id} photo={photo} />)
