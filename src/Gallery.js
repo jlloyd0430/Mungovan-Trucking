20c03bb1ab64672ff8a12626023adfc22b3abe4a
Jesse Lloyd <116366380+jlloyd0430@users.noreply.github.com>
2023-03-06 11:11:58 -0500

    Update Gallery.js

--- src/Gallery.js
+++ src/Gallery.js
@@ -16,16 +16,14 @@ function Gallery() {
           </p>
         </div>
         <div className="service">
-          <h2>Warehousing</h2>
-            <p>Our warehouse is where we store tools,deliverables and customer items and where we dispatch from.</p>
-        </div>
-        <div className="service">
-          <h2>Recycling</h2>
-          <p> when we cleanout trash or anything on any job, we seperate and recycle it all from cardboard, trash and steel right at our warehouse in Southbridge Ma. a convienient location in central Massachusetts</p>
+          <h2>Warehousing/Recycling</h2>
+            <p>Our warehouse is where we 
+dispatch from each day, we store  our tools,deliverables and customer items here, Aswell as seperate and recycle any cleanouts from jobs into cardboard, trash and steel right at our warehouse in Southbridge Ma. a convienient location in central Massachusetts.
+</p>
         </div>
            <div className="service">
           <h2>Verizon</h2>
-          <p> Looking for a certain peice part? did the engineer under order equipment for the job? maybe order the wrong type or size part? give us a call! we have been recycling peice parts saving the goods that may be thrown away at one job and being able to help out a tech in need at another job. we have anything from super structure and cable rack all the way to small peice parts. this part of our service comes with years of experience and being able to identify any part the customer may need exclusivly tailord for verizon customers.  </p>
+          <p> Looking for a certain peice part?Maybe under or over orderd equipment for the job? maybe order the wrong type or size part? give us a call! we recycle peice parts saving goods that may be thrown away at one job and being able to help out techs in need at another jobsite. we have anything from super structure, cable, cable-rack all the way to small peice parts. this part of our service comes from years of experience and is exclusivly tailord for verizon customers.  </p>
         </div>
       </div>
     </div>
