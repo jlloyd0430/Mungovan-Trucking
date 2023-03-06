c8f6fd533ea65a73ad20cc75892ff426ccd3f5e6
Jesse Lloyd <116366380+jlloyd0430@users.noreply.github.com>
2023-03-06 11:15:37 -0500

    Update Gallery.js

--- src/Gallery.js
+++ src/Gallery.js
@@ -16,13 +16,13 @@ function Gallery() {
           </p>
         </div>
         <div className="service">
-          <h2>Warehousing/Recycling</h2>
+          <h2>Warehousing</h2>
             <p>Our warehouse is where we 
 dispatch from each day, we store  our tools,deliverables and customer items here, Aswell as seperate and recycle any cleanouts from jobs into cardboard, trash and steel right at our warehouse in Southbridge Ma. a convienient location in central Massachusetts.
 </p>
         </div>
            <div className="service">
-          <h2>Verizon</h2>
+          <h2>Inventory</h2>
           <p> Looking for a certain peice part?Maybe under or over orderd equipment for the job? maybe order the wrong type or size part? give us a call! we recycle peice parts saving goods that may be thrown away at one job and being able to help out techs in need at another jobsite. we have anything from super structure, cable, cable-rack all the way to small peice parts. this part of our service comes from years of experience and is exclusivly tailord for verizon customers.  </p>
         </div>
       </div>
